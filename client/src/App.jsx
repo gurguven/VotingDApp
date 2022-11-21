538081ddda10c6cb79a6688e35a9434f55fa760d
Güven Gür <91473262+gurguven@users.noreply.github.com>
2022-11-21 21:41:21 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,6 @@ import OwnerDashboard from "./components/OwnerDashboard";
 import ProposalArray from "./components/ProposalArray"
 import Footer from "./components/Footer";
 import "./App.css";
-import Voters from "./components/Voters";
-
-
 
 function App() {
   const [status, setStatus] = useState("");
@@ -15,12 +12,9 @@ function App() {
     <EthProvider>
       <div id="App" >
           <div className="container">
-            {/* <Header  /> */}
             <Header status={status} setStatus={setStatus} />
             <div className="content">
-              {/* <OwnerDashboard/> */}
               <OwnerDashboard  status={status} setStatus={setStatus}/>
-              {/* <ProposalArray/> */}
               <ProposalArray status={status}/>
             </div>
             <Footer />
